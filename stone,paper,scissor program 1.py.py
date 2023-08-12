1d483553e359b37e3809cff65c6fdd964b322a73
Vijayananth16 <142078524+Vijayananth16@users.noreply.github.com>
2023-08-12 12:28:30 +0530

    initial commit

---
+++ stone,paper,scissor program 1.py.py
@@ -0,0 +1,19 @@
+
+import random
+user_choice=int(input("enter number between 0 to 2 ,0=rock,1=paper,2=scissor:"))
+comp_choice=random.randint(0,2)
+print("computer choice")
+print(comp_choice)
+if user_choice<3:
+ if user_choice==2 and comp_choice==0:
+    print("you lose")
+ elif user_choice==0 and comp_choice==2:
+    print("you win")
+ elif user_choice > comp_choice:
+    print("you win")
+ elif comp_choice > user_choice:
+    print("you lose")
+ elif user_choice==comp_choice:
+    print("match draw")
+else:
+    print("you enter wrong number")
